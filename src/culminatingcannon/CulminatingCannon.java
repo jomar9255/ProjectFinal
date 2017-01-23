a5d06d46b46899830c1f4f1da317d4dafbceb3b3
etwat3497 <atwatt67@gmail.com>
2017-01-23 13:44:28 -0500

    Update CulminatingCannon.java

--- src/culminatingcannon/CulminatingCannon.java
+++ src/culminatingcannon/CulminatingCannon.java
@@ -1,6 +1,7 @@
 
 package culminatingcannon;
 
+//Import decimal format
 import java.text.DecimalFormat;
 
 /**
@@ -13,33 +14,24 @@ public class CulminatingCannon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // DECLARATIONS / INITIALZATIONS
         
         //Decimal Format to give awnser to two decimal places
         DecimalFormat x = new DecimalFormat("##.##");
-        
+
         //Declare variables
         int numVar = 0;
         double v1, v1x, v1y, v2, v2y, dx, dy, t, a, theta1, theta2, solution;
         String unknownVar = "", varName = "";
         
         //Array holding the users variable values with each index corresponding to a specific variable
-        //double[] varValue = new double[numVar];
+        double[] varValue = new double[numVar];
         
-        //Testing
-        double[] varValue = {0,10,0,0,5,0,0,0,0,0,0};
-        unknownVar = "v2";
-        numVar = 2;
-        varName = "v1xv2y";
-
-        //In GUI the user will input info in alphabetical order and each variable they
-        //are using will be added to a megastring
 
         //Create a new decisionmaker object called awnser
         //Input the users unknown and known variables, the number of variables, and the value of all of them
         decisionMaker awnser = new decisionMaker(unknownVar, numVar, varName, varValue);
         
-        //Find the users unknown variable and store in solution var
+        //Find the users unknown variable and store in solution variable
         solution = awnser.decisionStatements();
         
         //Output the users solution
